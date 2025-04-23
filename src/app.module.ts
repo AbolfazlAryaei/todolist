835c14be7959ce3b13bd4c542505b6012c561192
Abolfazl-Aryaei <abolfazlaryaei803@gmail.com>
2025-04-23 18:40:43 +0330

    v1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,27 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 import { TaskModule } from './task/task.module';
 
 @Module({
   imports: [
-      TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [__dirname + '/**/*.entity.{js,ts}'],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'sqlite',
+        database: config.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity.{js,ts}'],
+        synchronize: true,
       }),
-      UserModule,
-      TaskModule
-    ],
+    }),
+
+    UserModule,
+    TaskModule,
+  ],
 })
 export class AppModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -6,13 +6,20 @@ import { User } from './entities/user.entity';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
     PassportModule,
-    JwtModule.register({
-      secret: '1234',
-      signOptions: { expiresIn: '1d' },
+
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRES_IN') },
+      }),
     }),
   ],
   providers: [UserService, JwtStrategy],
